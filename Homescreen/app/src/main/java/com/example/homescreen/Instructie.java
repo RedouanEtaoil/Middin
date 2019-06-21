2ce9f28f80056ea1a1e8f3ace1710b0a14ac41d9
RedouanEtaoil <44897096+RedouanEtaoil@users.noreply.github.com>
2019-06-21 10:20:27 +0200

    Instructie scherm toegevoegd

--- Homescreen/app/src/main/java/com/example/homescreen/Instructie.java
+++ Homescreen/app/src/main/java/com/example/homescreen/Instructie.java
@@ -2,7 +2,10 @@ package com.example.homescreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Instructie extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Instructie extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructie);
+
+        Button cButton= findViewById(R.id.button2);
+        cButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Instructie.this, Instructiescherm.class));
+
+            }
+        });
     }
 }
+
